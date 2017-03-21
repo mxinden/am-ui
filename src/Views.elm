a69365378b0b86c10a7e3f67ddbd9d8f78ac5868
Max Leonard Inden <IndenML@gmail.com>
2017-03-21 22:50:34 +0100

    Use import alias on qualified view imports
    Related to issue #8

--- src/Views.elm
+++ src/Views.elm
@@ -5,12 +5,12 @@ import Html.Attributes exposing (class)
 import Types exposing (Msg, Model, Route(..))
 import Utils.Types exposing (ApiResponse(..))
 import Utils.Views exposing (error, loading)
-import Views.SilenceList.Views
-import Views.SilenceForm.Views
-import Views.AlertList.Views
-import Views.Silence.Views
-import Views.NotFound.Views
-import Views.Status.Views
+import Views.SilenceList.Views as SilenceList
+import Views.SilenceForm.Views as SilenceForm
+import Views.AlertList.Views as AlertList
+import Views.Silence.Views as Silence
+import Views.NotFound.Views as NotFound
+import Views.Status.Views as Status
 import Views.NavBar.Views exposing (appHeader)
 
 
@@ -36,33 +36,33 @@ appBody : Model -> Html Msg
 appBody model =
     case model.route of
         StatusRoute ->
-            Views.Status.Views.view model
+            Status.view model
 
         SilenceRoute silenceId ->
-            Views.Silence.Views.view model
+            Silence.view model
 
         AlertsRoute route ->
             case model.alertGroups of
                 Success alertGroups ->
-                    Views.AlertList.Views.view route alertGroups model.filter (text "")
+                   AlertList.view route alertGroups model.filter (text "")
 
                 Loading ->
                     loading
 
                 Failure msg ->
-                    Views.AlertList.Views.view route [] model.filter (error msg)
+                    AlertList.view route [] model.filter (error msg)
 
         SilenceListRoute route ->
-            Views.SilenceList.Views.view model.silences model.silence model.currentTime model.filter
+            SilenceList.view model.silences model.silence model.currentTime model.filter
 
         SilenceFormNewRoute ->
-            Views.SilenceForm.Views.newForm model
+            SilenceForm.newForm model
 
         SilenceFormEditRoute silenceId ->
-            Views.SilenceForm.Views.editForm model
+            SilenceForm.editForm model
 
         TopLevelRoute ->
             Utils.Views.loading
 
         NotFoundRoute ->
-            Views.NotFound.Views.view
+            NotFound.view
