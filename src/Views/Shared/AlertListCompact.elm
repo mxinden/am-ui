a5d772752b29a41aaec2fcf798b4e13a8eb3cf1f
Max Leonard Inden <IndenML@gmail.com>
2017-03-21 23:25:22 +0100

    Refactor AlertListCompact.views function
    Credit goes to @w0rm.
    
    Related to issue #8

--- src/Views/Shared/AlertListCompact.elm
+++ src/Views/Shared/AlertListCompact.elm
@@ -5,18 +5,9 @@ import Html exposing (Html, ul)
 import Html.Attributes exposing (class)
 import Views.Shared.AlertCompact
 
-
 view : AlertGroup -> Html msg
-view ag =
-    let
-        alerts =
-            List.concatMap
-                (\b ->
-                    b.alerts
-                )
-                ag.blocks
-    in
-        ul
-            [ class "list pa0"
-            ]
-            (List.indexedMap Views.Shared.AlertCompact.view alerts)
+view { blocks } =
+  blocks
+   |> List.concatMap .alerts
+   |> List.indexedMap Views.Shared.AlertCompact.view
+   |> ul [ class "list pa0" ]
